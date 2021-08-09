01b3ab7293d92b899c8a86a447f8f73fc174155f
Agustin Isasa <tin.isasa@gmail.com>
2021-08-08 22:06:51 -0300

    add items to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,16 @@ import CartWidget from './CartWidget';
 
 const NavBar = ({ appName }) => {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-expand navbar-light bg-light">
             <span className="navbar-brand mb-0 h1">{appName}</span>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Catálogo</a>
+                </li>
+            </ul>
             <CartWidget />
         </nav>)
 }
