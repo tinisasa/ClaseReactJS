32e869e532885732a005bc79486a1505af8a3e8b
Agustin Isasa <tin.isasa@gmail.com>
2021-09-19 23:40:22 -0300

    fix 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>NotFound</h1>
+        <div className="container">
+            <p>Página no encontrada.</p>
+            <Link to="/">Volver al catálogo</Link>
         </div>
     );
 };
